edfa28efc1c30a92652f2bec7c64943f9aee3fbd
Gökalp Ateş <atessgokalp@gmail.com>
2021-03-24 13:00:25 +0300

    Initial commit.
    Initial commit to Github.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# E-Mail Sender
- Python script which sends same e-mail to selected receivers.
+This is a Python e-mail bot that sends e-mails to designated receivers.
+
+I got this idea from my deadly cumbersome internship applications.
+Sending a single e-mail for every enterprise was a huge and tiring job so,
+I decided to make a bot to send my e-mails automatically.
+If you are a HR employee, than you guessed right. E-mail that you received
+was sended by this bot.
+
+In project folder; Via plaintext.txt and html.html you can set your e-mail as 
+to contain a plain text and html or both. Specially, html file really helps me
+to support various features. In addition to that; you can send an attachment, if
+you specify a file in project folder.
+
+If you write null inside plaintext.txt or int html file, program will not attach
+it to the e-mail. My advise is attach both of them to your e-mails. If you wrote
+a html formatted text, browser will try to render it first. If it is not successful,
+browser will try to render plaintext one.
---
+++ main.py
@@ -0,0 +1,82 @@
+import smtplib
+import ssl
+import time
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+port = 465
+
+account = input("Mail address: ")
+password = input("Password: ")
+
+subject = input("Enter subject of your e-mails: ")
+
+receiver = ""
+
+message = MIMEMultipart("alternative")
+message["Subject"] = subject
+message["From"] = '<' + account + '>'
+
+f_text = open("plaintext.txt", "r")
+text = f_text.read()
+part1 = MIMEText(text, "plain")
+
+if text != "null":
+    message.attach(part1)
+else:
+    print("Notification: Plaintext file did not attached to mail message because of it has specified with null.")
+
+f_html = open("html.html", "r")
+html = f_html.read()
+part2 = MIMEText(html, "html")
+
+if html != "null":
+    message.attach(part2)
+else:
+    print("Notification: Html file did not attached to mail message because of it has specified with null.")
+
+attachmentFileName = input("Enter attachment name: ")
+
+with open(attachmentFileName, "rb") as attachment:
+    part3 = MIMEBase("application", "octet-stream")
+    part3.set_payload(attachment.read())
+
+encoders.encode_base64(part3)
+part3.add_header("Content-Disposition", f"attachment; filename= {attachmentFileName}")
+
+if attachmentFileName == "null":
+    print("Notification: Attachment file did not attached to mail message because of it has specified with null.")
+else:
+    message.attach(part3)
+
+print("Send e-mails? (type yes or no): ")
+isUserSure = input()
+if isUserSure == "no":
+    exit(0)
+elif isUserSure == "yes":
+    print("Sending e-mails...")
+else:
+    print("invalid input exiting from program")
+    exit(1)
+
+f_receivers = open("receivers.txt", "r")
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(account, password)
+    while f_receivers.readable():
+        receiver = f_receivers.readline()
+        message["To"] = '<' + receiver + '>'
+        server.sendmail(account, receiver, message.as_string())
+        print("Mail has sent to " + receiver)
+    server.quit()
+
+print("Sending completed successfully.")
+time.sleep(3)
+
+f_html.close()
+f_text.close()
+f_receivers.close()
